093a1ce5fc563392d162e5e0e666a85df452752a
Timur Sadorov <sadorov2001@mail.ru>
2021-05-12 21:29:40 +0300

    V0

---
+++ PUSHKA/Assets/Scenes/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private GameObject[] zombeis;
+
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
